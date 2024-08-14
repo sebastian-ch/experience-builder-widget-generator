5d29b58060e9158cdf3b690984f7af18d3477a9f
Sebastian Hancock <sebastianc.hancock@gmail.com>
2024-08-14 13:05:31 -0700

    first

---
+++ README.md
@@ -0,0 +1,29 @@
+# Widget Creator for ArcGIS Experience Builder
+
+    This node module generates a custom experience builder widget from your command line. You can choose any of the demos in [the official ArcGIS Experience Builder page](https://github.com/Esri/arcgis-experience-builder-sdk-resources/tree/master/widgets) as a template.
+## Installation
+
+Install the package globally:
+
+```bash
+npm install -g esri-widget-generator
+```
+
+
+go to your experience builder folder ``ArcGISExperienceBuilder\client\your-extensions\widgets``
+(it will install the folder anywhere though)
+
+
+run ``create-widget``
+give your widget a name and select the template
+
+![]("example.gif")
+
+
+
+
+that should be it!
+
+
+
+
---
+++ index.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { input, select } from '@inquirer/prompts';
+import fs from 'fs-extra';
+import path from 'path';
+import { Octokit } from 'octokit';
+import axios from 'axios';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const octokit = new Octokit();
+
+const REPO_OWNER = 'Esri';
+const REPO_NAME = 'arcgis-experience-builder-sdk-resources';
+const WIDGETS_PATH = 'widgets';
+
+async function getWidgets() {
+    try {
+        const { data } = await octokit.rest.repos.getContent({
+            owner: REPO_OWNER,
+            repo: REPO_NAME,
+            path: WIDGETS_PATH,
+        });
+
+        return data
+            .filter(item => item.type === 'dir')
+            .map(item => ({ value: item.name, label: item.name }));
+    } catch (error) {
+        console.error('Error fetching widgets:', error.message);
+        return [];
+    }
+}
+
+async function copyWidget(widget, destination) {
+    try {
+        const { data } = await octokit.rest.repos.getContent({
+            owner: REPO_OWNER,
+            repo: REPO_NAME,
+            path: `${WIDGETS_PATH}/${widget}`,
+        });
+
+        for (const item of data) {
+            if (item.type === 'file') {
+                const fileContent = await octokit.rest.repos.getContent({
+                    owner: REPO_OWNER,
+                    repo: REPO_NAME,
+                    path: item.path,
+                });
+
+                const content = Buffer.from(fileContent.data.content, 'base64').toString('utf-8');
+                await fs.outputFile(path.join(destination, item.name), content);
+            } else if (item.type === 'dir') {
+                await copyWidget(`${widget}/${item.name}`, path.join(destination, item.name));
+            }
+        }
+    } catch (error) {
+        console.error('Error copying widget:', error.message);
+    }
+}
+
+program
+    .name('create-widget')
+    .description('Generate a custom widget template for ArcGIS Experience Builder')
+    .version('1.0.0');
+
+program.action(async () => {
+    const widgets = await getWidgets();
+
+    const widgetName = await input({
+        message: 'Enter the new widget name:',
+        validate: input => input.trim() !== '' || 'Widget name is required'
+    });
+
+    const selectedWidget = await select({
+        message: 'Choose a widget template:',
+        choices: widgets
+    });
+
+    console.log(`Generating widget: ${widgetName} using ${selectedWidget} template`);
+
+    const widgetDir = path.join(process.cwd(), widgetName);
+
+    await copyWidget(selectedWidget, widgetDir);
+
+    // Modify package.json if it exists
+    const packageJsonPath = path.join(widgetDir, 'package.json');
+    if (await fs.pathExists(packageJsonPath)) {
+        const packageJson = await fs.readJson(packageJsonPath);
+        packageJson.name = widgetName;
+        await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+    }
+
+    console.log('Widget template created successfully!');
+});
+
+program.parse(process.argv);
